7e089ad771f6158de6fbffb16874c821eae92b99
dawa sherpa <sherpadawag1998@gmail.com>
2024-05-06 23:29:48 +0545

    refactor: header code changes for serach and badges

--- src/page/Layout.tsx
+++ src/page/Layout.tsx
@@ -1,17 +1,24 @@
 import React, { PropsWithChildren } from 'react'
-import Header from '../components/header'
-
-
+import { Badge } from '@mui/material'
+import GradeIcon from '@mui/icons-material/Grade';
 
 
 export const Layout = ({children}:PropsWithChildren) => {
     return (
     
-          
+          <div style={{padding:'20px'}}>
+            <div className='head'>
+                <input type='text' placeholder="search spell" />
+                 <Badge badgeContent={4} color="primary">
+                    <GradeIcon color="action" />
+                </Badge>
+            </div>
             <div className="layout-body">
 
             {children}
             </div>
+          </div>
+           
        
     )
 }
